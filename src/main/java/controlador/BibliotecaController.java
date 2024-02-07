f181986d1488f071df436d1bfc233331d2070b1b
Your Name <you@example.com>
2024-02-07 20:30:17 +0100

    Primer commit

---
+++ src/main/java/controlador/BibliotecaController.java
@@ -0,0 +1,75 @@
+package controlador;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import modelo.Libro;
+import modelo.LibroDAO;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Servlet implementation class BibliotecaController
+ */
+@WebServlet(urlPatterns ={"","/insertar"})
+public class BibliotecaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BibliotecaController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet BibliotecaController");
+		RequestDispatcher despachador = null;
+		if (request.getServletPath().equals("")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				ArrayList<Libro> libros;
+				libros = libroDAO.getLibros();
+				request.setAttribute("libros", libros);
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		} else if (request.getServletPath().equals("/insertar")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				Libro libro = new Libro(Integer.parseInt(request.getParameter("isbn")),
+						request.getParameter("titulo"),request.getParameter("autor"));
+				libroDAO.insertar(libro);
+				request.setAttribute("info", "Libro "+ libro +" añadido");
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/");
+		}
+		despachador.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,59 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="modelo.Libro" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Biblioteca</title>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<% String error = (String)request.getAttribute("error");
+	 	if (error!=null){ %>
+	 		<p><%=error%></p>
+	<%}%>
+
+	<% String info = (String)request.getAttribute("info");
+	 	if (info!=null){ %>
+	 		<p><%=info%></p>
+	<%}%>
+
+	<table> 
+   		<tr> 
+          <th><b>ISBN</b></th> 
+          <th><b>Título</b></th> 
+          <th><b>Autor</b></th> 
+         </tr>
+         <% ArrayList<Libro> libros=(ArrayList<Libro>)request.getAttribute("libros"); 
+	    	if (libros != null){	
+	         for(Libro l:libros){%> 
+	            <tr> 
+	                <td><%=l.getIsbn()%></td> 
+	                <td><%=l.getTitulo()%></td> 
+	                <td><%=l.getAutor()%></td> 
+	            </tr> 
+
+	         <%}
+	         }%> 
+    </table>  
+
+
+
+    <div>
+	  <form action="insertar" method="post">
+	    <label for="isbn">ISBN</label>
+	    <input type="text" id="isbn" name="isbn" placeholder="ISBN..">
+
+	    <label for="titulo">Título</label>
+	    <input type="text" id="titulo" name="titulo" placeholder="Titulo..">
+
+	    <label for="autor">Autor</label>
+	    <input type="text" id="autor" name="autor" placeholder="Autor..">
+
+	    <input type="submit" value="Submit">
+	  </form>
+	</div>
+</body>
+</html>
